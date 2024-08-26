5db5b9c857504f37aa8b9232a0193afeedc7cbd3
maikito12 <maicocharra01@gmail.com>
2024-08-26 11:35:02 -0300

    ejercicio 15 y 16

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+
+let numeros= [1,2,3,4,5]
+
+function arrayDuplicado (numeros)
+{
+    return arrayDuplicado=[...numeros,...numeros];
+}
+
+const duplicado=arrayDuplicado(numeros)
+
+console.log(duplicado)
+let nombre="trolitos"
+function cadenaInvertida (numeros)
+{
+   return numeros.reverse().join('');
+
+}
+
+let probando= cadenaInvertida(numeros)
+console.log(probando)
